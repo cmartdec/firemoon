d4f863d1ad4f34111c13a0ed239d0c5da4818cdb
cmartdec <cmartdec@gmail.com>
2022-05-12 23:41:18 +0200

    deleted testing files

--- client/src/testing/Testing.jsx
+++ client/src/testing/Testing.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import Amazon from '../assets/amazon.png'
 
 export default function Testing() {
 
 
   return (
     <>
-    <div className="h-[60px] w-full bg-[#131921]">
-    </div>
-      <div className="h-[40px] w-full bg-[#232E3E] flex items-center pl-4">
-        <p className="font-bold text-sm text-white">Todo</p>
 
-      </div>
     
     </>
     
