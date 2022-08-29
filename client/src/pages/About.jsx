89d7cc77bcf78d255beb7066b2d8fa560f5f36b2
cmartdec <cmartdec@gmail.com>
2022-08-29 12:36:31 +0200

    design

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Topbar from '../components/Topbar'
-import Topbar_dpl from '../components/Topbar_dpl'
 
 export default function About() {
   return (
       <>
-       <Topbar_dpl></Topbar_dpl>
+      <Topbar></Topbar>
 
       </>
   )
